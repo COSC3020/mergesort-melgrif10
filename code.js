a6087df9ad672d98cf63a04d4f8336218c8d47b1
melgrif10 <mgriff25@uwyo.edu>
2023-11-20 00:22:50 -0700

    Mergesort 11-20

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 function mergesort(array) {
+    if(array.length<=1){        //If the array has 0 or 1 elements
+        return array;       //No sorting needed, return array
+    }
+
     const len = array.length;   //length variable 
     var size;       //Used to represent the size of the subarrays
-    var leftSide;   //Keeps trakc of the begining of the left subarray
+    var leftSide;   //Keeps track of the begining of the left subarray
     for(size=1; size<len; size=size*2){     //Controls size of subarrays to be merged, subarray size starts with 1 and doubled each iteration
         for(leftSide=0; leftSide<len; leftSide+=2*size){    //The starting index of leftSide is 0, the "start" index then increases with each iteration by leftSide+2*size
             const middle=Math.min(leftSide+size-1, len-1);  //Determines the end of the left subarray and the begining of the right subarray. Math.min ensures it won't go past the last index of the array
@@ -9,15 +13,16 @@ function mergesort(array) {
             merge(array, leftSide, middle, rightEnd);   //Merges the left and right subarrays within the main array   
         }
     } 
+    return array;   //Return sorted array (added to pass tests)
 }
 
 function merge(array, leftSide, middle, rightEnd){      //Merge Function
+    if(array.length<=1 || leftSide>=rightEnd || middle>=rightEnd){
+        return;     //If the array is empty there is no need to sort, return
+    }
+    
     let leftEnd=middle;     //The end of the left subarray is the middle of the main array
     let rightSide=middle+1;     //The start of the right subarray in the index right after the middle of the main array
-
-    if(array[middle]<=array[rightSide]){    //Checks if the last element of the left subarray is <= the first element in the right array
-        return;     //If so the array is already sorted
-    }
 
     while(leftSide<=leftEnd && rightSide<=rightEnd){    //As long as there are elements in both the left and right subarrays
         if(array[leftSide]<=array[rightSide]){      //Is the element in the left subarry <= the element in the right subarray
